e563f4ad835dec5d1ce984701a06a9d7f0f1482a
Pradeep_Kumar_Rebbavarapu <wardengaming1807@gmail.com>
2023-10-15 19:03:16 +0530

    added postgress database

--- Server/db.js
+++ Server/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2"
+import pg from 'pg'
 
-export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password: "Shri@12345",
-  database:"game"
+export const db = new pg.Pool({
+  connectionString:'postgres://pullandpushgame_user:1Sd3w2aViSQ5uu9baXjErVCJJbjllCUl@dpg-cklu9g8u1l6c739oljs0-a.singapore-postgres.render.com/pullandpushgame' ,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 db.connect(function (err) {
